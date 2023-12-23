84ac6306db929e9b1b0f1f154d311a62f937f38e
Arunabh Das <arunabhdas@gmail.com>
2023-12-23 03:46:52 -0500

    Added HomeRepository

--- UnicornApp/composeApp/src/commonMain/kotlin/HomeRepository.kt
+++ UnicornApp/composeApp/src/commonMain/kotlin/HomeRepository.kt
@@ -1,3 +1,16 @@
-class HomeRepository {
+import apiClient.httpClient
+import data.Product
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import kotlinx.coroutines.flow.flow
 
+class HomeRepository {
+    suspend fun getProductsApi(): List<Product> {
+        val response = httpClient.get("https://fakestoreapi.com/products")
+        return response.body()
+    }
+
+    fun getProducts() = flow {
+        emit(getProductsApi())
+    }
 }
