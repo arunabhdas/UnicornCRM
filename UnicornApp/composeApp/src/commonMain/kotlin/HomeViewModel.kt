c89cf8c0309172ac638fcf6b4b6415939d981bb1
Arunabh Das <arunabhdas@gmail.com>
2023-12-23 04:12:54 -0500

    Added HomeViewModel

---
+++ UnicornApp/composeApp/src/commonMain/kotlin/HomeViewModel.kt
@@ -0,0 +1,24 @@
+import data.Product
+import dev.icerock.moko.mvvm.viewmodel.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class HomeViewModel : ViewModel() {
+    private val _products = MutableStateFlow(listOf<Product>())
+    val products = _products.asStateFlow()
+
+    private val homeRepository = HomeRepository()
+
+
+
+    init {
+        viewModelScope.launch {
+            homeRepository.getProducts().collect { products ->
+                _products.update { it + products }
+            }
+        }
+    }
+}
