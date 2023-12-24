a3c2c0d959dd9138da9a2905a806d1cacdf97e2e
Arunabh Das <arunabhdas@gmail.com>
2023-12-24 17:09:10 -0500

    Added Card UI for product

--- UnicornApp/composeApp/src/commonMain/kotlin/App.kt
+++ UnicornApp/composeApp/src/commonMain/kotlin/App.kt
@@ -1,9 +1,19 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -33,7 +43,38 @@ fun AppContent(homeViewModel: HomeViewModel) {
         var cols = 2
         var modifier = Modifier.fillMaxWidth()
         if (maxWidth > 840.dp) {
-            
+           cols = 3
+           modifier = Modifier.widthIn(max = 1080.dp)
+
+        }
+
+        val scrollState = rememberLazyGridState()
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(cols),
+                state = scrollState,
+                contentPadding = PaddingValues(16.dp)
+                ) {
+
+                items(
+                    items = products.value,
+                    key = {product ->
+                        product.id.toString()
+                    }) {
+                    Card(
+                        shape = RoundedCornerShape(15.dp),
+                        modifier = Modifier.padding(8.dp).fillMaxWidth(),
+                        elevation = 2.dp
+                    ) {
+                        // Image
+                    }
+                }
+            }
         }
     }
+
 }
