46b8f056ed7f7e3de768d7e95a1b69a170ad28b9
Arunabh Das <arunabhdas@gmail.com>
2023-12-24 20:18:37 -0500

    Added CompositionLocalProvider to iOS target MainViewController

--- UnicornApp/composeApp/src/iosMain/kotlin/MainViewController.kt
+++ UnicornApp/composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,5 +1,8 @@
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
 import com.seiko.imageloader.ImageLoader
+import com.seiko.imageloader.LocalImageLoader
 import com.seiko.imageloader.component.setupDefaultComponents
 import com.seiko.imageloader.defaultImageResultMemoryCache
 import okio.Path.Companion.toPath
@@ -7,7 +10,13 @@ import platform.Foundation.NSCachesDirectory
 import platform.Foundation.NSSearchPathForDirectoriesInDomains
 import platform.Foundation.NSUserDomainMask
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    CompositionLocalProvider(
+        LocalImageLoader provides remember { generateImageLoader() },
+    ) {
+        App()
+    }
+}
 
 fun generateImageLoader(): ImageLoader {
     return ImageLoader {
