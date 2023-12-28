bd1d202b5cccdaa4d751431d986ef2e7d7ff944b
Arunabh Das <arunabhdas@gmail.com>
2023-12-28 01:59:18 -0500

    Inject MainViewModel into MainActivity

--- unicorncrm-android/app/src/main/java/app/unicornapp/unicorncrm/MainActivity.kt
+++ unicorncrm-android/app/src/main/java/app/unicornapp/unicorncrm/MainActivity.kt
@@ -17,17 +17,18 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.NavHostController
 import app.unicornapp.unicorncrm.presentation.DefaultViewModel
+import app.unicornapp.unicorncrm.presentation.MainViewModel
 import app.unicornapp.unicorncrm.ui.screens.NavGraphs
 import app.unicornapp.unicorncrm.ui.theme.UnicornTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class MainActivity : ComponentActivity() {
     lateinit var navHostController: NavHostController
     private val viewModel: DefaultViewModel by viewModels()
+    private val mainViewModel by viewModel<MainViewModel>()
 
     @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
