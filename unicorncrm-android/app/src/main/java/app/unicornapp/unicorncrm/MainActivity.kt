a05affc28a0b8a806398ade2f5f55d8471672dbe
Arunabh Das <arunabhdas@gmail.com>
2025-03-23 05:24:20 -0400

    Added ObserveAsEvents function for Snackbar events

--- unicorncrm-android/app/src/main/java/app/unicornapp/unicorncrm/MainActivity.kt
+++ unicorncrm-android/app/src/main/java/app/unicornapp/unicorncrm/MainActivity.kt
@@ -8,6 +8,10 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.ui.Modifier
 import androidx.core.app.ActivityCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -20,6 +24,10 @@ import app.unicornapp.unicorncrm.ui.theme.UnicornTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import org.koin.androidx.viewmodel.ext.android.getViewModel
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
@@ -36,12 +44,40 @@ class MainActivity : ComponentActivity() {
             }
         }
         setContent {
-
             UnicornTheme {
+                val snackbarHostState = remember {
+                   SnackbarHostState()
+                }
+                val scope = rememberCoroutineScope()
+
+                ObserveAsEvents(
+                    flow = SnackbarManager.events,
+                    key1 = snackbarHostState
+                    ) { event ->
+                    scope.launch {
+                        snackbarHostState.currentSnackbarData?.dismiss()
+                        val result = snackbarHostState.showSnackbar(
+                            message = event.message,
+                            actionLabel = event.action?.name,
+                            duration = SnackbarDuration.Long
+                        )
+                        if (result == SnackbarResult.ActionPerformed) {
+                            event.action?.action?.invoke()
+                        }
+                    }
+                }
                 val viewModel = getViewModel<MainViewModel>()
                 viewModel.doNetworkCall()
                 navHostController = rememberNavController()
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    snackbarHost = {
+                        SnackbarHost(
+                            hostState = snackbarHostState
+                        )
+                    }
+
+                ) { innerPadding ->
                     DestinationsNavHost(navGraph = NavGraphs.root)
                 }
 
