c5c5dc85dab22484d1bf2b8a626a7f81be6e04ea
Arunabh Das <arunabhdas@gmail.com>
2025-03-25 06:15:00 -0400

    Added Movie domain data class

--- unicorncrm-android/app/src/main/java/app/unicornapp/unicorncrm/movielist/domain/model/Movie.kt
+++ unicorncrm-android/app/src/main/java/app/unicornapp/unicorncrm/movielist/domain/model/Movie.kt
@@ -1,8 +1,9 @@
-package app.unicornapp.unicorncrm.movielist.data.local
+package app.unicornapp.unicorncrm.movielist.domain.model
+
 
 import androidx.room.PrimaryKey
 
-data class MovieEntity(
+data class Movie(
     val adult: Boolean,
     val backdrop_path: String,
     val genre_ids: List<Int>,
@@ -16,8 +17,6 @@ data class MovieEntity(
     val video: Boolean,
     val vote_average: Double,
     val vote_count: Int,
-
-    @PrimaryKey
     val id: Int,
     val category: String
 )
