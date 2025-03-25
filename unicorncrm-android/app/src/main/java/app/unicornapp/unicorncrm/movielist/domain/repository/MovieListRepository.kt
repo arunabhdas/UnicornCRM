5510ffa6c83a710306984f8f301c84a7ad7d15ce
Arunabh Das <arunabhdas@gmail.com>
2025-03-25 06:26:19 -0400

    Created util classes

---
+++ unicorncrm-android/app/src/main/java/app/unicornapp/unicorncrm/movielist/domain/repository/MovieListRepository.kt
@@ -0,0 +1,12 @@
+package app.unicornapp.unicorncrm.movielist.domain.repository
+
+import app.unicornapp.unicorncrm.movielist.util.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface MovieListRepository {
+    suspend fun getMovieList(
+        forceFetchFromRemote: Boolean,
+        category: String,
+        page: Int
+    ): Flow<Resource>
+}
---
+++ unicorncrm-android/app/src/main/java/app/unicornapp/unicorncrm/movielist/util/Category.kt
@@ -0,0 +1,6 @@
+package app.unicornapp.unicorncrm.movielist.util
+
+object Category {
+    const val POPULAR = "popular"
+    const val UPCOMING = "upcoming"
+}
