fa60c71d976eba45c3ea000a544788fbc4b0c167
Arunabh Das <arunabhdas@gmail.com>
2025-03-25 06:28:23 -0400

    Added MovieListRepository interface

--- unicorncrm-android/app/src/main/java/app/unicornapp/unicorncrm/movielist/domain/repository/MovieListRepository.kt
+++ unicorncrm-android/app/src/main/java/app/unicornapp/unicorncrm/movielist/domain/repository/MovieListRepository.kt
@@ -2,11 +2,12 @@ package app.unicornapp.unicorncrm.movielist.domain.repository
 
 import app.unicornapp.unicorncrm.movielist.util.Resource
 import kotlinx.coroutines.flow.Flow
+import app.unicornapp.unicorncrm.movielist.domain.model.Movie
 
 interface MovieListRepository {
     suspend fun getMovieList(
         forceFetchFromRemote: Boolean,
         category: String,
         page: Int
-    ): Flow<Resource>
+    ): Flow<Resource<List<Movie>>>
 }
