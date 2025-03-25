488a70d1a806413c17a9120bc48f968422f7eb16
Arunabh Das <arunabhdas@gmail.com>
2025-03-25 05:52:44 -0400

    Created MovieDao

--- unicorncrm-android/app/src/main/java/app/unicornapp/unicorncrm/movielist/data/local/MovieDao.kt
+++ unicorncrm-android/app/src/main/java/app/unicornapp/unicorncrm/movielist/data/local/MovieDao.kt
@@ -1,8 +1,14 @@
 package app.unicornapp.unicorncrm.movielist.data.local
 
+import androidx.room.Query
 import androidx.room.Upsert
 
 interface MovieDao {
     @Upsert
-    suspend fun upsertMovieList(movieList: )
+    suspend fun upsertMovieList(movieList: List<MovieEntity>)
+
+    @Query("SELECT * FROM MovieEntity WHERE id = :id")
+    suspend fun getMovieById(id: Int): MovieEntity
+
+
 }
