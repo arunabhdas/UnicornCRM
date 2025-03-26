653aa21d758be828d35fa8efa087ddb6addd7b81
Arunabh Das <arunabhdas@gmail.com>
2025-03-26 07:23:51 -0400

    Created MovieListState and MoviesViewModel

--- unicorncrm-android/app/src/main/java/app/unicornapp/unicorncrm/movielist/presentation/MovieListState.kt
+++ unicorncrm-android/app/src/main/java/app/unicornapp/unicorncrm/movielist/presentation/MovieListState.kt
@@ -4,8 +4,8 @@ import app.unicornapp.unicorncrm.movielist.domain.model.Movie
 
 data class MovieListState (
     val isLoading: Boolean = false,
-    val popularMovieListPage: Int = 0,
-    val upcomingMoviewListPage: Int = 1,
+    val popularMovieListPage: Int = 1,
+    val upcomingMovieListPage: Int = 1,
     val isCurrentScreenPopular: Boolean = true,
     val popularMovieList: List<Movie> = emptyList(),
     val upcomingMovieList: List<Movie> = emptyList()
