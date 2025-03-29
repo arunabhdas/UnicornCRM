54414aac82424ada9d39bdf011ef09906ae807cf
Arunabh Das <arunabhdas@gmail.com>
2025-03-29 06:20:33 -0400

    Added route for popular movies

--- unicorncrm-android/app/src/main/java/app/unicornapp/unicorncrm/ui/navigation/ScreenDrawer.kt
+++ unicorncrm-android/app/src/main/java/app/unicornapp/unicorncrm/ui/navigation/ScreenDrawer.kt
@@ -2,6 +2,7 @@ package app.unicornapp.unicorncrm.ui.navigation
 
 sealed class ScreenDrawer {
     object MoviesScreen: Screen(route = "movies_screen")
+    object MoviesPopularScreen: Screen(route = "movies_popular_screen")
     object RatesScreen: Screen(route = "rates_screen")
     object SettingsScreen: Screen(route = "settings_screen")
     object NotificationScreen: Screen(route = "notification_screen")
