356a98587286e13d4c9e1edeb52c6d9ce78be924
Arunabh Das <arunabhdas@gmail.com>
2025-03-31 07:19:21 -0400

    Added title for MoviesUpcomingScreen

--- unicorncrm-android/app/src/main/java/app/unicornapp/unicorncrm/ui/screens/MoviesUpcomingScreen.kt
+++ unicorncrm-android/app/src/main/java/app/unicornapp/unicorncrm/ui/screens/MoviesUpcomingScreen.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -17,8 +19,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
@@ -80,6 +85,20 @@ fun MoviesUpcomingScreen(
             ),
         contentAlignment = Alignment.Center
     ) {
+        // Add a title at the top of the screen
+        Text(
+            text = "Upcoming",
+            modifier = Modifier
+                .fillMaxWidth()
+                .statusBarsPadding()
+                .padding(top = 16.dp)
+                .align(Alignment.TopCenter),
+            color = Color.White,
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+
         if (movieListState.isLoading && movieListState.upcomingMovieList.isEmpty()) {
             CircularProgressIndicator(
                 modifier = Modifier.align(Alignment.Center),
